279a02567d54014c6c3049acc42af3573b20d568
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2023-01-25 11:12:29 +0930

    finish auth part

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+	statusCode = 401;
+
+	constructor(public message: string) {
+		super('Not authorized');
+
+		Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+	}
+	serializeErrors() {
+		return [{ message: 'Not authorized' }];
+	}
+}
+
+// new RequestValidationError(errors);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 const router = express.Router();
 
 router.post(
@@ -13,12 +15,9 @@ router.post(
 			.trim()
 			.isLength({ min: 4, max: 20 })
 			.withMessage('Password must between 4 to 20 characters'),
-	],
+	],validateRequest,
 	async (req: Request, res: Response) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			throw new RequestValidationError(errors.array());
-		}
+	
 		const { email, password } = req.body;
 		const existingUser = await User.findOne({ email });
 		if (existingUser) {
@@ -26,6 +25,17 @@ router.post(
 		} else {
 			const user = User.build({ email, password });
 			await user.save();
+
+			//jwt save in session
+			const userJwt = jwt.sign(
+				{
+					id: user.id,
+					email: user.email,
+				},
+				process.env.JWT_KEY!
+			);
+
+			req.session = { jwt: userJwt };
 			return res.status(201).send(user);
 		}
 	}
